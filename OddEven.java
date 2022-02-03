47cbae5782ed329fea96c0f14f8527514020b86b
KeionT <93876343+KeionT@users.noreply.github.com>
2022-02-03 16:13:48 -0500

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -10,8 +10,14 @@ public class OddEven {
         //If number is divisable by 2 then its a even number
         //Else odd number
         if( num % 2 == 0 )
-        System.out.println("Not Cool! Entered number is even");
+            System.out.println("Not Cool! Entered number is even");
         else
-        System.out.println("Cool! Entered number is odd ");
+            System.out.println("Cool! Entered number is odd ");
+        if ( num => 2 & =<5 )
+            System.out.println("Not Cool! Entered number is even");
+        if ( num => 6 & =< 20 )
+            System.out.println("Not Cool! Entered number is even");
+        if ( num > 20)
+            System.out.println("Not Cool! Entered number is even");   
     }
 }

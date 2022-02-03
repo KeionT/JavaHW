51ac5dddd7525669efe9ba841354fa6df4c2ea95
KeionT <93876343+KeionT@users.noreply.github.com>
2022-02-03 16:05:28 -0500

    1st

---
+++ OddEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String args[]){
+        int num;
+        System.out.println("Enter a Number: ");
+        //input provided by user is stored in num
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        //If number is divisable by 2 then its a even number
+        //Else odd number
+        if( num % 2 == 0 )
+        System.out.println("Not Cool! Entered number is even");
+        else
+        System.out.println("Cool! Entered number is odd ");
+    }
+}
